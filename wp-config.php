360d9aad27a0583da44a0d2589dcd696dcf81571
Space Rabbit <34202174+Ujutokki@users.noreply.github.com>
2024-02-06 13:21:32 +0900

    Add DB info into wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'koyebdb' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'koyeb-adm' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'jzqM4ynGrR8m' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'postgres://koyeb-adm:jzqM4ynGrR8m@ep-dark-butterfly-a4t8izyt.us-east-1.pg.koyeb.app/koyebdb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
